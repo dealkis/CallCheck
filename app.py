ccdc81bdfba67570466d6120c02788456bf69933
dealkis <emanoelc18@gmail.com>
2026-05-04 12:03:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def conectar():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")"
+        database=os.getenv("DB_NAME")
     )
 
 # =========================
