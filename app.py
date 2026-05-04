539a5e0d051e8674235eff6e408e65e4afff4161
dealkis <emanoelc18@gmail.com>
2026-05-04 11:52:53 -0300

    Update database connection to use environment variables

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -8,11 +9,11 @@ app = Flask(__name__)
 # CONEXÃO
 # =========================
 def conectar():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="callcheck"
+      return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")"
     )
 
 # =========================
