98dd54a067c7c6ff45f8c6fe21f8f96c9c3f8e79
dealkis <emanoelc18@gmail.com>
2026-05-04 15:06:52 -0300

    Add contato route and update app structure

--- app.py
+++ app.py
@@ -99,6 +99,10 @@ def login():
 def sobre():
     return render_template("sobre.html")
 
+@app.route("/contato")
+def sobre():
+    return render_template("contato.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
